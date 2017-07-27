5dba31d523580146c71290238e9d7704ef6d1151
Thiago Yure Vieira de Almeida <thigoyure@gmail.com>
2017-07-27 16:53:53 -0300

    Algumas coisinhas a mais e correção de linkagem...

---
+++ www/js/album.js
@@ -0,0 +1,26 @@
+const user = firebase.auth().currentUser;
+const dbRefObjec = firebase.database().ref();
+const dbRefList = dbRefObjec.child('user/'+user.uid+'/albuns');
+for (var i = dbRefList.length - 1; i >= 0; i--) {
+	document.getElementById("albuns").innerHTML('<div class="card album">
+              <div class="card-image">
+                <img src="img/paisagem.jpg">
+                <span class="card-title">'dbRefList[i].name'</span>
+              </div>
+              <div class="card-action">
+                <a>Entrar</a>
+                <a class="btn-floating green activator waves-effect waves-light btn"><i class="material-icons">mode_edit</i></a>
+                <a class="btn-floating blue waves-effect waves-light"><i class="material-icons">delete</i></a>
+              </div>
+              <div class="card-reveal">
+                <span class="card-title grey-text text-darken-4">Editar Albúm<i class="material-icons right">close</i></span>
+                <form>
+                  <div class="input-field">
+                    <input class="validate" type="text" id="nomeAlbum">
+                    <label for="nomeAlbum">Novo nome</label>
+                  </div>
+                  <input type="submit" class="editar btn green waves-effect waves-light btn" value="Salvar">
+                </form>
+              </div>
+            </div>');
+};
